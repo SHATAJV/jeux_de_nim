7636ce51596a1586bc8c42c82c3f78ed908d1c37
shahrzad Tajvidi <shahrzad.tajvidi@fms-ea.com>
2024-07-18 13:43:47 +0200

    function de game

---
+++ nim_game_human.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Jeux de Nim (variante simple et de Marienbad)
+
+"""
+
+def play():
+
+    player1 = input("Enter Your name: ")
+    player2 = input("Enter Your name: ")
+    current_player = input("who want to start game(enter the name): ").strip().lower()
+
+    pile = 21
+    if current_player==player1:
+      print(f"it's your turn{player1}")
+    else:
+        print(f"it's your turn{player2}")
+    while True :
+         print(f"there is {pile} piles")
+         choice= input((f"Enter How many piles you want to take(1_4):"))
+         try:
+             if 1<choice<=4  and choice<=pile :
+
+                break
+             else:
+                 print("you didn't enter right choice please right again")
+         except ValueError:
+             print(f"Error ! try again")
+    pile -= choice
+    if pile==0:
+        print(f"You Loose {current_player}!")
+
+
